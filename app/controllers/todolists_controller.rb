8ff9ce7ac396e1b83fbcadb8ff19d126ae70f373
Niki Kakuwa <haru_knk@yahoo.co.jp>
2020-02-08 15:10:24 +0900

    [Finish]3-9

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -12,16 +12,29 @@ class TodolistsController < ApplicationController
     end
 
     def index
-      @lists = List.all
-      end
+        @lists = List.all
+    end
 
     def show
         @list = List.find(params[:id])
-      end
+    end
+
+    def edit
+        @list = List.find(params[:id])
+    end
+
+    def update
+        list = List.find(params[:id])
+        list.update(list_params)
+        redirect_to todolist_path(list.id)
+    end
+
+    def destroy
+    end
 
     private
 
     def list_params
-        params.require(:list).permit(:title, :body)
+       params.require(:list).permit(:title, :body, :image)
     end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b1a6e1ba5e73578241f8a2f78e6c043a82ce25966defdbe094076eb90f5e95268778c559a798281a91bda3f31783324610b927eadc848960a380648c68fd3cfc'
